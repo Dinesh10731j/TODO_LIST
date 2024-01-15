03dac1da217115f2aa1591ba1a4f0b90d67eef8b
Dinesh10731j <110474051+Dinesh10731j@users.noreply.github.com>
2024-01-15 16:14:38 +0545

    git

---
+++ script.js
@@ -0,0 +1,44 @@
+const item = document.querySelector("#item");
+const listItems = document.querySelector("#listItems");
+const num = document.querySelector(".num")
+
+let totalList = 0;
+let removeList = 1;
+
+
+item.addEventListener("keyup",(e)=>{
+    e.stopPropagation()
+   if(e.key == "Enter"){
+getInputValues(e.target.value);
+num.innerHTML = `Number of list:${totalList+1}`;
+item.value = null;
+   
+   } 
+
+ 
+});
+
+
+const getInputValues  = (item) => {
+
+    const listVal = document.createElement("li");
+    listVal.innerHTML =`${item} 
+    <i class="fas  fa-times"></i>
+    `
+listItems.appendChild(listVal);
+
+listVal.addEventListener("click",function(e){
+    e.stopPropagation();
+
+    this.classList.toggle("done");
+
+});
+
+listVal.querySelector("i").addEventListener("click",function(e){
+    e.stopPropagation()
+    listVal.remove();
+    num.innerHTML = `Number of list:${removeList--}`;
+  
+})
+
+}
